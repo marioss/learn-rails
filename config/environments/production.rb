5519b21b195576970fb532982cc4277746ef551a
Mario Stefano <me@mario.sg>
2013-11-08 09:28:46 +0700

    Fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ LearnRails::Application.configure do
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.perform_deliveries = true 
   config.action_mailer.raise_delivery_errors = false 
-  config.action_mailer.default :charset => "utf-8" 
+  config.action_mailer.default charset: "utf-8" 
   config.action_mailer.smtp_settings = {
     address: "smtp.mandrillapp.com",
     port: 587,
@@ -91,5 +91,5 @@ LearnRails::Application.configure do
     enable_starttls_auto: true, 
     user_name: ENV["MANDRILL_USERNAME"], 
     password: ENV["MANDRILL_PASSWORD"]
-  }￼￼
+  }
 end
